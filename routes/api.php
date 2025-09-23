9f83c78c8999e2f376e271e722a4ee218ada69fb
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-09-23 22:40:02 +0300

    t

--- routes/api.php
+++ routes/api.php
@@ -70,6 +70,7 @@ Route::middleware(['checkLocale'])->prefix('v1/{locale}')->group(function () {
         Route::post('', [PostController::class, 'store']);
         Route::get('{post}', [PostController::class, 'show']);
         Route::put('{post}', [PostController::class, 'update']);
+        Route::delete('{post}', [PostController::class, 'destroy']);
     });
 
     Route::prefix('post-comments')->group(function () {
